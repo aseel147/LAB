bb2ceff1ba88249d817957e9b9ec6e9f19fccd22
aseel147 <62061423+aseel147@users.noreply.github.com>
2020-03-15 12:12:19 +0200

    Update test.c

--- test.c
+++ test.c
@@ -1,1 +1,10 @@
 // test file
+//to p.50-51
+# define BaseAddres 0x40000000;
+# define AFIO (BaseAddres+10000) ;
+# define RCC (BaseAddres+0X21000) ;
+# define GPIO (BaseAddres+11000) ;
+# define USART (BaseAddres+138000) ;
+//manual 27.6
+USART-SR
+
