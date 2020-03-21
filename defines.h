467caa49c29c64118497ca5845bc0dd253c08c2b
Shir Granit <62025130+shirgra@users.noreply.github.com>
2020-03-22 00:00:23 +0200

    Add files via upload

--- defines.h
+++ defines.h
@@ -1,58 +1,32 @@
-// test file
-//to p.50-51
-# define BaseAddres 0x40000000;
-# define AFIO (BaseAddres+10000) ;
-# define RCC (BaseAddres+0X21000) ;
-# define GPIO (BaseAddres+11000) ;
-# define USART (BaseAddres+138000) ;
-//manual 7.2
-typedef struct{
-  uint32_t RCC_CR;//0x00 control register
-  uint32_t RCC_CFGR;//0x04 Clock configuration register
-  uint32_t RCC_CIR;//0x08 Clock interrupt register 
-  uint32_t RCC_APB2RSTR;//0x0C APB2 peripheral reset register 
-  uint32_t RCC_ABPB1RSTR;//0x10 APB1 peripheral reset register
-  uint32_t RCC_AHBENR;//0x14 AHB peripheral clock enable register
-  uint32_t  RCC_APB2ENR;//0x18 APB2 peripheral clock enable register 
-  uint32_t RCC_APB1ENR;//0x1C  APB1 peripheral clock enable register 
-  uint32_t RCC_BDCR;//0x20  Backup domain control register 
-  uint32_t RCC_CSR;//0x24 Control/status register
-}RCC_S;
-//MANUAL9.4 P.183-..
-typedef struct{
-  uint32_t AFIO_EVCR ;//Event control register 0X00
-  uint32_t  AFIO_MAPR;  //AF remap and debug I/O configuration register 0X04
-  uint32_t   AFIO_EXTICR1 ;//External interrupt configuration register1 0X08
-  uint32_t    AFIO_EXTICR2;//External interrupt configuration register 2 0X0C
-  uint32_t    AFIO_EXTICR3; //External interrupt configuration register 3 0X10
-  uint32_t AFIO_EXTICR4;//External interrupt configuration register 4 0x14
-  uint32_t AFIO_MAPR2; //AF remap and debug I/O configuration register2 0X1C
-}AFIO_S;//***********
-typdef struct{
-//manual 9.2
-uint32_t PIOx_CRL; //0x00 Port configuration register low
-uint32_t GPIOx_CRH; //0x04 Port configuration register high
-uint32_t GPIOx_IDR; //0x08Port input data register 
-uint32_t GPIOx_ODR; //0x0cPort output data register 
-uint32_t GPIOx_BSRR; //0x10ort bit set/reset register
-uint32_t GPIOx_BRR; //0x14 Port bit reset register
-uint32_t GPIOx_LCKR; //0x18Port configuration lock register 
-}GPIO_S;
-//manual 27.6.1
-typdef struct{
-//manual 9.2
-uint32_t USART_SR; //0x00 Status register
-uint32_t USART_DR; //0x04 Data register 
-uint32_t USART_BRR; //0x08 Baud rate register
-uint32_t USART_CR1; //0x0c Control register 1
-uint32_t  USART_CR2; //0x10 Control register 2
-uint32_t USART_CR3; //0x14 Control register 3
-uint32_t USART_GTPR ; //0x18 Guard time and prescaler register
-}USART_S;
- 
+#ifndef DEF
+#define DEFINITIONS
+#include <stdint.h>
+
+// Definitions:
+//RCC:
+# define RCC_BaseAdd 		0x40021000;
+# define RCC_APB2RSTR		0x4002100C;		//APB2 peripheral reset register;
+# define RCC_APB2ENR		0x40021018;  	//APB2 peripheral clock enable register;
+//GPIO_C:
+# define GPIO_C_BaseAdd 0x40011000;
+# define GPIO_C_CRH 		0x40011004;		//Port configuration register high; Reset value: 0x4444 4444;
+# define GPIO_C_ODR			0x4001100C;		//Port output data register
+# define GPIO_C_BSRR		0x40011010;		//Port bit set/reset register
+# define GPIO_C_BRR			0x40011014;		//Port bit reset register
+//GPIO_B:
+# define GPIO_B_BaseAdd 0x40010C00;
+# define GPIO_B_CRL 		0x40010C00;		//Port configuration register low;
+//USART:
+# define USART_BaseAdd  0x40013800;	
+# define USART_DR  			0x40013800;		//Data register [8:0]
+# define USART_SR  			0x40013804;		//tells us if data entered to USART_DR
+# define USART_BRR  		0x40013808;		//Baud rate register
+# define USART_CR1  		0x4001380C;		//Control register 1
+//AFIO:
+# define AFIO_BaseAdd  0x40010000;	
+# define AFIO_MAPR		  0x40010004;		//MAP USART to GPIO B IO.
 
 
-
-
-
+#endif
+//END
 
--- main.c
+++ main.c
@@ -1,14 +1,90 @@
-#include "stdio.h"
+#include "defines.h"
+#include <stdint.h>
 
+//MAIN:
 int main(void)
 {
-  char ONE_ ='1';
-  char ZER0_='0';
-  while(1)
-  {
-    
-  }
-  
-    
+	//Variables declare:
+	uint32_t* address_RCC_APB2ENR=	(uint32_t*)RCC_APB2ENR;
+	uint32_t* address_GPIO_C_CRH=		(uint32_t*)GPIO_C_CRH;
+	uint32_t* address_GPIO_C_BRR=		(uint32_t*)GPIO_C_BRR;
+	uint32_t* address_GPIO_C_BSRR=	(uint32_t*)GPIO_C_BSRR;
+	uint32_t* address_GPIO_B_CRL=		(uint32_t*)GPIO_B_CRL;
+	uint32_t* address_USART_CR1=		(uint32_t*)USART_CR1;
+	uint32_t* address_USART_BRR=		(uint32_t*)USART_BRR;
+	uint32_t* address_USART_DR=			(uint32_t*)USART_DR;
+	uint32_t* address_USART_SR=			(uint32_t*)USART_SR;
+	uint32_t* address_AFIO_MAPR=		(uint32_t*)AFIO_MAPR;
+	//define HELP variables:
+	char send_one=0;									//set manually!!!!!
+	char send_zero='0';							//set manually!!!!!
+	uint32_t dataIO;
+		
+	//Frist configurations:
+	//CLOCK Enables:
+	*address_RCC_APB2ENR=0x00004019;	//USART1EN=1,IOPCEN=1,IOPBEN=1,AFIOEN=1;
+	//port C enable:
+	*address_GPIO_C_CRH=0x44544444;		//MODE13=01 to turn led on;
+	//USART set:
+	*address_USART_CR1=0x0000200C;		//UE=1,RE=1,TE=1;
+	*address_USART_BRR=0x00000341;		//fraction=x1, mantissa=x0034;
+	*address_USART_SR|=0x000001C0;
+	//AFIO set:
+	*address_AFIO_MAPR=0x00000004;		//USART1_REMAP=1;
+	//port B set:
+	*address_GPIO_B_CRL=0x49000000;		//CNF7=01,MODE7=00,CNF6=10,MODE6=01;
+	
+	/////////////////////////////////////////////////////////////////////////
+	
+	//infinite while loop:
+	while(1){	
+		//check if recieve data in USART SR RXNE bit == 1:
+		if(*address_USART_SR&0x20)
+		{
+			//if recieve, check DR and SET dataIO to 1 or 0:
+			//if DR=='1':
+			if(*address_USART_DR=='1')
+			{
+			dataIO=1;
+			}
+			//if DR=='0':
+			if(*address_USART_DR=='0')
+			{
+			dataIO=0;
+			}
+			//CHECK dataIO:
+			if(dataIO==1){
+				//TURN LIGHT ON:
+				*address_GPIO_C_BRR=0x2000;
+			}
+			else if(dataIO==0){
+				//TURN LIGHT OFF:
+				*address_GPIO_C_BSRR=0x2000;
+			}
+			//reset USART SR RXNE bit5 to 0 (because we recieved data):
+			*address_USART_SR^=0x00000020;				//XOR from 1 to 0
+		}
+		//if data was not recived- check what to send:
+		if(send_one=='1')
+		{
+			//put in DR the value of '1':
+			*address_USART_DR=0x0110001;
+			//wait to end of Tx:
+			while (!(*address_USART_SR&0x00000080));
+			//set to polling:
+			send_one=0;
+		}
+		else if(send_zero=='0')
+		{
+			//put in DR the value of '0':
+			*address_USART_DR=0x0110000;
+			//wait to end of Tx:
+			while (!(*address_USART_SR&0x00000080));
+			//set to polling:
+			send_zero=0;
+		}
+	}
 }
-void cofige
+
+		
+	
